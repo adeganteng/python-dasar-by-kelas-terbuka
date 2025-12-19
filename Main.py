752022f4166a996f8a64ac3a24e992a9f8d865aa
adeganteng <wicaksonoade21@gmail.com>
2025-12-19 07:03:03 +0700

    Cara kerja Program dan Bytecode

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
+import time
+start_time = time.time()
 print("Hello World")
+print("Hello")
+print("World")
+print("Hello Guyss!")
+
+# Ini adalah comment jadi tidak akan di eksekusi oleh programnya
+a = 10 # Ini adalah variable + assignments kode hanya disimpan dalam memory dan akan dijalankan ketika sudah ada printahnya
+"""
+Ini adalah multiline comment
+Multi line comment juga tidak akan dijalankan oleh program
+"""
+print(a)
+
+for i in range(1, 1000):
+    a = 10
+
+print(time.time() - start_time, "detik")
+# Kita bisa mengcompile python ke yang namanya bytecode
