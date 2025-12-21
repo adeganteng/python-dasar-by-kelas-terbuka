048a7d7a80a23d7e6b0c019e7ac07acb751b4947
adeganteng <wicaksonoade21@gmail.com>
2025-12-21 11:24:59 +0700

    Episode 4: Variabel

---
+++ 4-Variabel/Main.py
@@ -0,0 +1,35 @@
+# Variabel adalah tempat untuk menyimpan data
+
+# A. Menaruh / Assignment nilai
+a = 10
+""" 
+"a" => Variabel
+"=" => Assignment
+"10" => Value
+"""
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print(a) # 10
+print("Nilai a = ", a) # Nilai a =  10
+print("Nilai x = ", x) # Nilai x =  5
+print("Nilai panjang = ", panjang) # Nilai panjang =  1000
+
+
+# B. Penamaan
+nilai_y = 15 # dengan menggunakan underscore "_"
+juta10 = 10000000 # Kalo angka tidak boleh ditaruh di paling depan, harus ditaruh ditengah atau paling belakang
+nilaiZ = 17.5 #huruf kecil dulu digabung dengan hruf besar
+
+# Pemanggilan kedua
+print("Nilai a = ", a) # Nilai a =  10
+a = 7
+print("Nilai a = ", a) # Nilai a =  7
+
+
+# C. Assigment Indirect
+b = a
+print("Nilai b = ", b) # Nilai b = 7
+
+
