3993e0826027b523ba8264da1d71ad543e998399
adeganteng <wicaksonoade21@gmail.com>
2025-12-21 16:59:17 +0700

    Episode 7: Mengambil Input Data Dari User

---
+++ 7-MengambilInputDataDariUser/Main.py
@@ -0,0 +1,17 @@
+#  Input User
+
+# Data yang dimasukkan pasti string
+data = input("Masukan data: ")
+print("Data: ", data, ", Tipe: ", type(data))
+
+# Jika ingin mengambil int, maka
+angka_float = float(input("Masukan Angka Desimal: "))
+print("Data: ", angka_float, ", Tipe: ", type(angka_float))
+
+angka = int(input("Masukan Angka: "))
+print("Data: ", angka, ", Tipe: ", type(angka))
+
+# Bagaimana Dengan Boolean
+biner = bool(int(input("Masukan nilai boolean (0/1): ")))
+print("Data: ", biner, ", Tipe: ", type(biner))
+
